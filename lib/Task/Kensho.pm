99a01a3e81a742e0871c1286d2d0ab6d1ef9bcfc
Chris Nehren <apeiron@cpan.org>
2010-10-24 01:23:07 -0400

    Fix Changes for release date, bump version

--- lib/Task/Kensho.pm
+++ lib/Task/Kensho.pm
@@ -1,6 +1,6 @@
 package Task::Kensho;
 use strict;
-our $VERSION = '0.25';
+our $VERSION = '0.26';
 
 __END__
 
@@ -12,7 +12,7 @@ Task::Kensho - A Glimpse at an Enlightened Perl
 
 =head1 VERSION
 
-This document describes Task::Kensho version 0.24
+This document describes Task::Kensho version 0.26
 
 =head1 SYNOPSIS
 
