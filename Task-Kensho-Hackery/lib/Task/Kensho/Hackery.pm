79b5dc39f632a5a24e9c158814725202940ae6ff
Karen Etheridge <ether@cpan.org>
2014-01-21 18:33:56 -0800

    perl generate-tasks for 0.34003

--- Task-Kensho-Hackery/lib/Task/Kensho/Hackery.pm
+++ Task-Kensho-Hackery/lib/Task/Kensho/Hackery.pm
@@ -3,7 +3,7 @@ use warnings;
 package {{
 ($module = $dist->name) =~ s/-/::/g;
 $module }};
-# ABSTRACT: {{ $config_data->{$module}{description} }}
+# ABSTRACT: A Glimpse at an Enlightened Perl: {{ $config_data->{$module}{description} }}
 
 __END__
 =pod
@@ -24,51 +24,63 @@ Kenshō (見性) (C. Wu) is a Japanese term for enlightenment
 experiences - most commonly used within the confines of Zen
 Buddhism - literally meaning "seeing one's nature"[1] or "true
 self."[2] It generally "refers to the realization of nonduality of
+subject and object."[3]
 
 =back
 
-Task::Kensho is a first cut at building a list of recommended modules
+L<Task::Kensho> is a first cut at building a list of recommended modules
 for Enlightened Perl development. CPAN is wonderful, but there are too
 many wheels and you have to pick and choose amongst the various
 competing technologies.
 
-The plan is for Task::Kensho to be a rough testing ground for ideas that
+The plan is for L<Task::Kensho> to be a rough testing ground for ideas that
 go into among other things the Enlightened Perl Organisation Extended
 Core (EPO-EC).
 
-The modules that are bundled by Task::Kensho are broken down into
+The modules that are bundled by L<Task::Kensho> are broken down into
 several categories and are still being considered. They are all taken
 from various top 100 most used perl modules lists and from discussions
 with various subject matter experts in the Perl Community. That said,
 this bundle does I<not> follow the guidelines established for the EPO-EC
 for peer review via industry advisers.
 
-Starting in 2011, Task::Kensho split its sub-groups of modules into
-individually-installable tasks. These individual tasks will always install all
-their modules by default. This facilitates the ease and simplicity the
-distribution aims to achieve. Each Task::Kensho sub-task is listed at the
+Starting in 2011, L<Task::Kensho> split its sub-groups of modules into
+individually-installable tasks.  Each L<Task::Kensho> sub-task is listed at the
 beginning of its section in this documentation.
 
-=head2 {{ $config_data->{$module}{description} . ': ' . $module
+When installing L<Task::Kensho> itself, you will be asked to install each
+sub-task in turn, or you can install individual tasks separately. These
+individual tasks will always install all their modules by default. This
+facilitates the ease and simplicity the distribution aims to achieve.
+
+=head1 RECOMMENDED MODULES
+
+=head2 {{ 'L<' . $module . '>: ' . $config_data->{$module}{description}
     . ($config_data->{$module}{long_description} ? ("\n\n" . $config_data->{$module}{long_description}) : '')
 }}
-
-=over 4
 
 {{
     join "\n\n",
         map {
-            '=item L<' . $_ . ">\n\n" . $config_data->{$module}{components}{$_}
+            '=head3 L<' . $_ . ">\n\n" . $config_data->{$module}{components}{$_}
         } sort keys %{ $config_data->{$module}{components} }
 }}
 
-=back
+=head1 INSTALLING
+
+Since version 0.34, L<Task::Kensho> has made use of the C<optional_features> field
+in distribution metadata. This allows CPAN clients to interact with you
+regarding which modules you wish to install.
+
+The C<cpanm> client requires interactive mode to be enabled for this to work:
+
+    cpanm --interactive Task-Kensho
 
 =head1 RELEASE SCHEDULE
 
-Starting with release 0.18 Task::Kensho was moved to a monthly release
+Starting with release 0.18, L<Task::Kensho> was moved to a monthly release
 cycle. This will facilitate a consistent schedule for upstream vendors
-to track the changes in Task::Kensho.
+to track the changes in L<Task::Kensho>.
 
 =head1 BUGS AND LIMITATIONS
 
@@ -79,8 +91,7 @@ better for you. This is however a path to good perl practice, and a
 starting place on the road to Enlightened Perl programming.
 
 Please report any bugs or feature requests to
-C<bug-task-kensho@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
+L<{{ $dist->distmeta->{resources}{bugtracker}{web} }}>.
 
 =head1 SEE ALSO
 
