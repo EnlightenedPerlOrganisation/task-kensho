31576fb316f72413f310c7e46c9a2c61a03c2fa9
Karen Etheridge <ether@cpan.org>
2014-01-30 11:17:04 -0800

    shell functions for testing and releasing

---
+++ release.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+tk_test()
+{
+    dzil test --release
+    if [[ $? -ne 0 ]]; then
+        echo "uh oh something went wrong; testing halted"
+        return 1
+    fi
+    for f in Task-Kensho-[A-Z]*/
+    do
+        pushd $f
+        dzil test --release
+        if [[ $? -ne 0 ]]; then
+            echo "uh oh something went wrong; testing halted"
+            popd
+            return 1
+        fi
+        popd
+    done
+}
+
+tk_release()
+{
+    for f in Task-Kensho-[A-Z]*/
+    do
+        pushd $f
+        dzil release
+        if [[ $? -ne 0 ]]; then
+            echo "uh oh something went wrong; releases halted"
+            popd
+            return 1
+        fi
+        popd
+    done
+    dzil release
+}
