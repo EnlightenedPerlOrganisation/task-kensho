ca8d354cb660f6dd607035e5672fae48e40b1517
Karen Etheridge <ether@cpan.org>
2015-07-18 13:25:49 -0700

    update subtask build to reflect changes made to my bundle between 0.068 and 0.099

--- inc/SubTaskPluginBundle.pm
+++ inc/SubTaskPluginBundle.pm
@@ -3,18 +3,18 @@ use warnings;
 package # stay away, PAUSE!
     inc::SubTaskPluginBundle;
 # ABSTRACT: A plugin bundle for building Task::Kensho:: subdists
-# vim: set ts=8 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 use 5.014;  # s///r
 use Moose;
 with
     'MooseX::SimpleConfig',
     'Dist::Zilla::Role::PluginBundle::Easy',
-    'Dist::Zilla::Role::PluginBundle::PluginRemover' => { -version => '0.102' },
+    'Dist::Zilla::Role::PluginBundle::PluginRemover' => { -version => '0.103' },
     'Dist::Zilla::Role::PluginBundle::Config::Slicer',
     'Dist::Zilla::Role::BundleDeps';
 
-use Devel::CheckBin;
+use Devel::CheckBin 'can_run';
 use namespace::autoclean;
 
 # this is ripped off wholesale from Dist::Zilla::PluginBundle::Author::ETHER
@@ -55,7 +55,7 @@ sub configure
 
     my @plugins = (
         # VersionProvider
-        [ 'Git::NextVersion'    => { version_regexp => '^v([\d._]+)(-TRIAL)?$' } ],
+        [ 'RewriteVersion' => { ':version' => '0.004', global => 1, add_tarball_name => 0 } ],
 
         # Gather Files
         [ 'Git::GatherDir'      => { ':version' => '2.016', exclude_match => '^inc', exclude_filename => [ 'dist.ini', 'modules.yml', 'META.json', 'README.md', 'README.pod' ] } ],
@@ -63,7 +63,7 @@ sub configure
         qw(MetaYAML MetaJSON License Manifest),
         [ 'Test::Compile'       => { ':version' => '2.039', bail_out_on_fail => 1, xt_mode => 1 } ],
         'Test::NoTabs',
-        'EOLTests',
+        'Test::EOL',
         'MetaTests',
         [ 'Test::CPAN::Changes' => { ':version' => '0.008' } ],
         'Test::ChangesHasContent',
@@ -71,26 +71,27 @@ sub configure
         'PodSyntaxTests',
         'PodCoverageTests',
         [ 'Test::PodSpelling'   => { ':version' => '2.006003' } ],
-        'Test::Kwalitee',
-        'MojibakeTests',
+        # 'Test::Pod::No404s',  # TODO: see RT#105937
+        [ 'Test::Kwalitee'      => { ':version' => '2.06', filename => 'xt/author/kwalitee.t' } ],
+        [ 'MojibakeTests'       => { ':version' => '0.8' } ],
         [ 'Test::ReportPrereqs' => { ':version' => '0.019', verify_prereqs => 1 } ], # gives us something in t/
         'Test::Portability',
-        'Test::CleanNamespaces',
+        [ 'Test::CleanNamespaces' => { ':version' => '0.006' } ],
 
         # Munge Files
-        'Git::Describe',
-        [ PkgVersion            => { ':version' => '5.010', die_on_existing_version => 1, die_on_line_insertion => 1 } ],
-        [ 'Authority'           => { authority => 'cpan:PERIGRIN', do_munging => 0} ],
-        [ PodWeaver             => { ':version' => '4.005', replacer => 'replace_with_comment', post_code_replacer => 'replace_with_nothing' } ],
+        [ 'Git::Describe'       => { ':version' => '0.004', on_package_line => 1 } ],
+        [ PodWeaver             => { ':version' => '4.005', config_plugin => '@Author::ETHER', replacer => 'replace_with_comment', post_code_replacer => 'replace_with_nothing' } ],
         [ 'NextRelease'         => { ':version' => '4.300018', time_zone => 'UTC', format => '%-8v  %{yyyy-MM-dd}d%{ (TRIAL RELEASE)}T' } ],
 
         # MetaData
         [ 'GithubMeta'          => { issues => 1 } ],
-        # (Authority)
+        [ 'AuthorityFromModule' => { ':version' => '0.002' } ],
+        [ 'Authority'           => { ':version' => '1.009', authority => 'cpan:PERIGRIN', do_munging => 0 } ],
         [ 'MetaNoIndex'         => { directory => [ qw(t xt examples share) ] } ],
-        [ 'MetaProvides::Package' => { meta_noindex => 1, ':version' => '1.15000002', finder => ':InstallModules' } ],
+        [ 'MetaProvides::Package' => { ':version' => '1.15000002', finder => ':InstallModules', meta_noindex => 1, inherit_version => 0, inherit_missing => 0 } ],
         'MetaConfig',
         [ 'Git::Contributors'   => { ':version' => '0.006', order_by => 'commits', paths => [ '.', '../modules.yml' ] } ],
+        # (StaticInstall)
 
         # Register Prereqs
         'AutoPrereqs',
@@ -101,11 +102,12 @@ sub configure
             } ],
 
         # Test Runner
-        [ 'RunExtraTests'       => { ':version' => '0.019' } ],
+        [ 'RunExtraTests'       => { ':version' => '0.024' } ],
 
         # Install Tool
-        [ 'MakeMaker::Fallback'   => { ':version' => '0.012' } ],
-        [ 'ModuleBuildTiny::Fallback' => { ':version' => '0.006' } ],
+        [ 'MakeMaker::Fallback' => { ':version' => '0.012' } ],
+        [ 'ModuleBuildTiny::Fallback' => { ':version' => '0.011', version_method => 'conservative' } ],
+        [ 'StaticInstall'       => { ':version' => '0.005', mode => 'auto' } ],
 
         # After Build
         'CheckSelfDependency',
@@ -119,7 +121,7 @@ sub configure
         'CheckPrereqsIndexed',
         'TestRelease',
         [ 'Git::Check'          => 'after tests' => { repo_root => '..', allow_dirty => [''] } ],
-        [ 'CheckIssues' ],
+        'CheckIssues',
         # note: no [ConfirmRelease]
 
         # Releaser
@@ -127,9 +129,9 @@ sub configure
         [ 'ArchiveRelease'      => { directory => '../released-subtasks' } ],
 
         # After Release
-        #[ 'CopyFilesFromRelease' => { filename => [ 'META.json' ] } ],
-        [ 'Git::Commit'         => { ':version' => '2.020', add_files_in => ['.'], allow_dirty => [ 'Changes' ], commit_msg => '%N-%v%t%n%n%c' } ],
-        # note: no [Git::Tag], [Git::Push]
+        [ 'Git::Commit'         => 'release snapshot' => { ':version' => '2.020', add_files_in => ['.'], allow_dirty => [ 'Changes',  ], commit_msg => '%N-%v%t%n%n%c' } ],
+        # note: no [Git::Tag], [Git::Push], [BumpVersionAfterRelease], [NextRelease], [Git::Commit]
+
         [ 'Clean' ],
     );
 
